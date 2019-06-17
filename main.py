4d550108a289541960e9b7a08559bae7dbf0cf1b
zyr17 <jzjqz17@gmail.com>
2019-06-17 22:17:13 +0800

    CNNpart

--- main.py
+++ main.py
@@ -15,7 +15,7 @@ train_label = np.zeros((len(train_label_raw), label_num), dtype='float')
 for num in range(len(train_label_raw)):
     train_label[num][train_label_raw[num] - 1] = 1
 with open('data/pickle/train_image.pkl', 'rb') as f:
-    train_image = pickle.load(f) # TODO: use image
+    train_image = pickle.load(f)
 
 with open('data/pickle/test_visit.bucket.pkl', 'rb') as f:
     test_visit = pickle.load(f)
@@ -81,108 +81,79 @@ class FC(torch.nn.Module):
                 x = self.ReLU(linear(x))
         return x # [B, outputlen]
 
+#feature_length, kernel_size, padding, maxpool_size, maxpool_padding
+image_input = (3, 100, 100)
+image_CNN = (
+    (50, 5, 2, 2, (0, 0)),
+    (200, 5, 2, 2, (0, 0)),
+    (400, 3, 1, 3, (1, 1)),
+    (800, 3, 1, 3, (0, 0)),
+)
+image_FC = (800 * 3 * 3, 240)
+visit_input = (7, 26, 24)
+visit_CNN = (
+    (100, 5, 2, 2, (0, 0)),
+    (200, 5, 2, 2, (1, 0)),
+    (400, 5, 2, 2, (1, 0)),
+    (800, 5, 2, 2, (0, 1)),
+)
+visit_FC = (800 * 2 * 2, 160)
+
+class CNNpart(torch.nn.Module):
+    def __init__(self, inputlen, CNN, FC):
+        super(CNNpart, self).__init__()
+        self.CNN = CNN
+        self.FC = FC
+        self.visit_convs = torch.nn.ModuleList()
+        self.visit_fcs = torch.nn.ModuleList()
+        lastfea = inputlen[0]
+        for i in CNN:
+            self.visit_convs.append(torch.nn.Sequential(
+                torch.nn.Conv2d(lastfea, i[0], i[1], padding=i[2]),
+                torch.nn.ReLU(),
+                torch.nn.MaxPool2d(i[3], padding=i[4])
+            ))
+            lastfea = i[0]
+        lastfea = FC[0]
+        for i in FC[1:]:
+            self.visit_fcs.append(torch.nn.Sequential(
+                torch.nn.Linear(lastfea, i),
+                torch.nn.ReLU()
+            ))
+            lastfea = i
+    def forward(self, inputs):
+        x = inputs
+        for conv in self.visit_convs:
+            x = conv(x)
+        x = x.reshape(-1, self.FC[0])
+        for fc in self.visit_fcs:
+            x = fc(x)
+        return x
+
 class CNN(torch.nn.Module):
     def __init__(self, inputlen = (7, 26, 24), outputlen = label_num):
         super(CNN, self).__init__()
-        self.visit_convs = torch.nn.ModuleList()
-        self.visit_fcs = torch.nn.ModuleList()
-        self.visit_convs.append(torch.nn.Sequential(
-            torch.nn.Conv2d(inputlen[0], 100, 5, padding=2),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2)
-        ))# [B, 100, 13, 12]
-        self.visit_convs.append(torch.nn.Sequential(
-            torch.nn.Conv2d(100, 200, 5, padding=2),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2, padding=(1, 0))
-        ))# [B, 200, 7, 6]
-        self.visit_convs.append(torch.nn.Sequential(
-            torch.nn.Conv2d(200, 400, 5, padding=2),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2, padding=(1, 0))
-        ))# [B, 400, 4, 3]
-        self.visit_convs.append(torch.nn.Sequential(
-            torch.nn.Conv2d(400, 800, 5, padding=2),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2, padding=(0, 1))
-        ))# [B, 800, 2, 2]
-        self.visit_fcs.append(torch.nn.Sequential(
-            torch.nn.Linear(800 * 2 * 2, 160),
-            torch.nn.ReLU()
-        ))# [B, 160]
-        self.visit_fcs.append(torch.nn.Sequential(
+        self.CNNpart = CNNpart(visit_input, visit_CNN, visit_FC)
+        self.fc.append(torch.nn.Sequential(
             torch.nn.Linear(160, outputlen),
             torch.nn.Sigmoid()
         ))# [B, 9]
     def forward(self, inputs, images):
         x = inputs # [B, 7, 26, 24]
-        for conv in self.visit_convs:
-            x = conv(x)
-        x = x.reshape(-1, 800 * 2 * 2)
-        for fc in self.visit_fcs:
-            x = fc(x)
+        x = self.CNNpart(x)
+        x = self.fc(x)
         return x
+
 #TODO: MaxPool2d在不整除时行为
 class concat_after(torch.nn.Module):
-    def __init__(self, visitlen = (7, 26, 24), imagelen = (3, 100, 100), outputlen = label_num):
+    def __init__(self, outputlen = label_num):
         super(concat_after, self).__init__()
-        self.visit_convs = torch.nn.ModuleList()
-        self.visit_fcs = torch.nn.ModuleList()
-        self.visit_convs.append(torch.nn.Sequential(
-            torch.nn.Conv2d(visitlen[0], 100, 5, padding=2),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2)
-        ))# [B, 100, 13, 12]
-        self.visit_convs.append(torch.nn.Sequential(
-            torch.nn.Conv2d(100, 200, 5, padding=2),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2, padding=(1, 0))
-        ))# [B, 200, 7, 6]
-        self.visit_convs.append(torch.nn.Sequential(
-            torch.nn.Conv2d(200, 400, 5, padding=2),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2, padding=(1, 0))
-        ))# [B, 400, 4, 3]
-        self.visit_convs.append(torch.nn.Sequential(
-            torch.nn.Conv2d(400, 800, 5, padding=2),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2, padding=(0, 1))
-        ))# [B, 800, 2, 2]
-        self.visit_fcs.append(torch.nn.Sequential(
-            torch.nn.Linear(800 * 2 * 2, 160),
-            torch.nn.ReLU()
-        ))# [B, 160]
-
-        self.image_convs = torch.nn.ModuleList()
-        self.image_fcs = torch.nn.ModuleList()
-        self.image_convs.append(torch.nn.Sequential(
-            torch.nn.Conv2d(imagelen[0], 50, 5, padding=2),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2)
-        ))# [B, 50, 50, 50]
-        self.image_convs.append(torch.nn.Sequential(
-            torch.nn.Conv2d(50, 200, 5, padding=2),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(2)
-        ))# [B, 200, 25, 25]
-        self.image_convs.append(torch.nn.Sequential(
-            torch.nn.Conv2d(200, 400, 3, padding=1),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(3, padding=1)
-        ))# [B, 400, 9, 9]
-        self.image_convs.append(torch.nn.Sequential(
-            torch.nn.Conv2d(400, 800, 3, padding=1),
-            torch.nn.ReLU(),
-            torch.nn.MaxPool2d(3)
-        ))# [B, 800, 3, 3]
-        self.image_fcs.append(torch.nn.Sequential(
-            torch.nn.Linear(800 * 3 * 3, 240),
-            torch.nn.ReLU()
-        ))# [B, 240]
+        self.imageCNN = CNNpart(image_input, image_CNN, image_FC)
+        self.visitCNN = CNNpart(visit_input, visit_CNN, visit_FC)
 
         self.final_fcs = torch.nn.ModuleList()
         self.final_fcs.append(torch.nn.Sequential(
-            torch.nn.Linear(160 + 240, 80),
+            torch.nn.Linear(image_FC[-1] + visit_FC[-1], 80),
             torch.nn.ReLU()
         ))# [B, 80]
         self.final_fcs.append(torch.nn.Sequential(
@@ -191,18 +162,10 @@ class concat_after(torch.nn.Module):
         ))# [B, 9]
     def forward(self, inputs, images):
         x = inputs # [B, 7, 26, 24]
-        for conv in self.visit_convs:
-            x = conv(x)
-        x = x.reshape(-1, 800 * 2 * 2)
-        for fc in self.visit_fcs:
-            x = fc(x)
+        x = self.visitCNN(x)
         
         y = images
-        for conv in self.image_convs:
-            y = conv(y)
-        y = y.reshape(-1, 800 * 3 * 3)
-        for fc in self.image_fcs:
-            y = fc(y)
+        y = self.imageCNN(y)
         
         z = torch.cat([x, y], 1)
         for fc in self.final_fcs:
@@ -246,7 +209,7 @@ train_loader = torch.utils.data.DataLoader(train_dataset, batch_size, True)
 test_dataset = VisitDataset(test_visit, test_image, np.zeros(len(test_visit)))
 test_loader = torch.utils.data.DataLoader(test_dataset, batch_size, False)
 val_dataset = VisitDataset(val_visit, val_image, val_label)
-val_loader = torch.utils.data.DataLoader(val_dataset, 100, True)
+val_loader = torch.utils.data.DataLoader(val_dataset, batch_size, True)
 if (modelname == 'CNN'):
     model = CNN()
 elif (modelname == 'FC'):
